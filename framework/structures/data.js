a1f155af4877f909dd4a420ae861b4363c6c039a
Glenn B <55507853+grber1234@users.noreply.github.com>
2021-10-14 19:22:54 -0700

    Initial commit

---
+++ framework/structures/data.js
@@ -0,0 +1,11 @@
+/**
+ * @file data.js
+ * @module data
+ * @description Contains the singleton data structure definition that allows the entire application,
+ * to share data between various components of the framework and greater application platform.
+ * @author Glenn Butler
+ * @date 2021/10/13
+ * @copyright Copyright © 2021-… by Glenn Butler. All rights reserved
+ */
+
+export var data = {};
